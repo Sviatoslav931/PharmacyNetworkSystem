3c2816cf1bc6e91cd410c66a86f120f8ed59e3ec
Andriy Diedyk <diedyk.andriy@gmail.com>
2016-10-16 20:45:33 +0300

    Delete SQLQuery4.sql

--- PharmaDatabase/SQLQuery4.sql
+++
@@ -1,8 +0,0 @@
-SELECT Medicaments.medicamentName, COUNT(Products.productID)
-FROM Medicaments
-LEFT JOIN Products
-ON Products.medicamentID = Medicaments.medicamentID
-GROUP BY Medicaments.medicamentName
-;
-
-
